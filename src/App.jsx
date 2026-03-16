400c88af49a0884d55026736a0021bda5772c578
Meegu80 <aceman80@nate.com>
2026-03-16 11:28:36 +0900

    Ver1.0 - toDo List 기본형 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import {useState} from "react";
+
 const App = () => {
-  
-// 렌더링 파트(JSX & TSX = 태그를 사용할 수 있게해주는 구간)=================================================================== 
+    // todo리스트 1건에 대한 입력
+    const [toDo, setToDo] = useState("");
+    // todo리스트가 저장된 todos(todo전체목록)에 대한 입력
+    const [toDos, setToDos] = useState([]);
+
+    // 입력란에 무언가 변화가 일어난다면(글자가 입력/삭제) 그 안에 생선된 객체 event의 value값을 setToDo함수에 넣어준다는 뜻
+    // setTodo는 toDo의 값을 결정하는 함수이기때문에 setTodo(event.target.value)를 통해서 toDo의 값이 event.target.value가 된다는 뜻이다.
+    const onChange = (event) => setToDo(event.target.value);
+
+    //
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        } else {
+            setToDos([...toDos, toDo]);
+        }
+    }
+
+
+// 렌더링 파트(JSX & TSX = 태그를 사용할 수 있게해주는 구간)===================================================================
     return (
         <div>
-            App
+            <h1>My Todos({toDos.length}개)</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text"
+                       value={toDo}
+                       onChange={onChange}
+                       placeholder="오늘 할 일을 적어주세요"/>
+                <button>추가하기</button>
+            </form>
+            <hr/>
+            <ul>
+
+                {toDos.map((items, index) => (
+                    <li key={index}>{items}</li>
+                ))}
+            </ul>
         </div>
     )
 }
